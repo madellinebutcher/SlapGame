d294870643c803f41cca21a21d79ebc425fcf3d5
Madelline Butcher <madellinebutcher@gmail.com>
2018-04-27 16:01:36 -0600

    updated

--- game.js
+++ game.js
@@ -3,16 +3,24 @@
 // var hits = 0;
 
 var meowth = new Target('Meowth')
-var backpack = {
-    paralyzePotion: new Item("Paralyze Potion", 5, "It is Paralyzed.")
-    
 
+var backpackItems= {
+    paralyzePotion: new Item("Paralyze Potion", 5, "It cannot move."),
+    pokeToy: new Item("Poke Toy", 10, "It is distracted."),
+    xAttack: new Item("X Attack", 15, "Increased attack power.")
 }
 
 function Target(name){
     this.name = name;
     this.health = 100;
     this.hits = 0;
+    this.items= [];
+
+}
+
+function giveItem(itemName){
+    meowth.items.push(backpackItems[itemName])
+    //where you are pushing.push(where it goes)
 }
 
 function Item(name, modifier, description){
@@ -46,5 +54,5 @@ function drawUpdate() {
 
 
 }
-
+giveItem()
 drawUpdate()
