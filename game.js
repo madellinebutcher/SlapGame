02a74d8cf876a78d8d375562545a8b3a84785bc6
Madelline Butcher <madellinebutcher@gmail.com>
2018-04-27 12:11:10 -0600

    updated

--- game.js
+++ game.js
@@ -1,19 +1,30 @@
 var health = 100;
+var target = "Meowth";
+var hits = 0;
 
 function quickAttack() {
     health--
     //alert(health);
+    hits++
     drawUpdate()
 }
 function thunderBolt() {
-    health--
+    health = (health-5)
     //alert(health);
+    hits++
     drawUpdate()
 }
-
+function ironTail() {
+    health = (health-10)
+    //alert(health);
+    hits++
+    drawUpdate()
+}
 function drawUpdate() {
 
     document.getElementById("health").innerText = health;
+    document.getElementById("hits").innerText = hits;
+
 
 }
 
